7285aea07c887f82db10183094f1ff97cb184d8e
Daniel Eklund <doeklund@gmail.com>
2011-12-15 11:38:20 -0800

    dictionary trie initialization with leinengen

---
+++ src/dictionary_trie/core.clj
@@ -0,0 +1,1 @@
+(ns dictionary-trie.core)
---
+++ test/dictionary_trie/test/core.clj
@@ -0,0 +1,6 @@
+(ns dictionary-trie.test.core
+  (:use [dictionary-trie.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
